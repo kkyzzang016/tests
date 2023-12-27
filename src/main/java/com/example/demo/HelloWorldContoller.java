32e7867da2c84d88f8b7dfa2eab7a2fb12de026b
kkyzzang016 <kkyzzang016@nate.com>
2023-12-27 18:12:23 +0900

    Update HelloWorldContoller.java

--- src/main/java/com/example/demo/HelloWorldContoller.java
+++ src/main/java/com/example/demo/HelloWorldContoller.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @GetMapping("/")
