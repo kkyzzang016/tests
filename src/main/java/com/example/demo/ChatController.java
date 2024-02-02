20ac444d458fca96295f24aae70908e2239c9fa2
84962 <kkyzzang016@nate.com>
2024-02-02 13:48:54 +0900

    dd

---
+++ src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculateController {
+
+    @GetMapping("chat")
+    public String chat() {
+
+        return "chatInterface";
+    }
+
+
+}
