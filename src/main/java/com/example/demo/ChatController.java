16330a54767418f4b30bf486de893f7c27763dca
84962 <kkyzzang016@nate.com>
2024-02-02 13:50:42 +0900

    dd

--- src/main/java/com/example/demo/ChatController.java
+++ src/main/java/com/example/demo/ChatController.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class CalculateController {
 
     @GetMapping("chat")
